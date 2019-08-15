de049ef5ee24e078b9c1054efce17ba40f46f6cd
alex echaniz <10alex.echa@gmail.com>
2019-08-15 21:40:43 +0200

    using mongo

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/webhook', (req, res) => {
 
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
-   
+        console.log(JSON.stringify(webhook_event))
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
         if (webhook_event.message) {
