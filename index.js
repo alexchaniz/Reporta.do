7333bd18e7332b4ca2301d889c5ae6745aa62caa
alex echaniz <10alex.echa@gmail.com>
2019-08-28 03:02:16 +0200

    klk

--- index.js
+++ index.js
@@ -736,9 +736,9 @@ function sendToArcGis(update) {
   console.log(object);
 
   try {
-    // stringObject = JSON.stringify(object);
+    stringObject = JSON.stringify(object);
 
-    var url = 'https://services1.arcgis.com/C4QnL6lJusCeBpYO/arcgis/rest/services/PruebaPuntos/FeatureServer/0/addFeatures?f=JSON&features=' + JSON.stringify(object);
+    var url = 'https://services1.arcgis.com/C4QnL6lJusCeBpYO/arcgis/rest/services/PruebaPuntos/FeatureServer/0/addFeatures?f=JSON&features=' + stringObject;
     Http.open("POST", url);
     Http.send();
 
