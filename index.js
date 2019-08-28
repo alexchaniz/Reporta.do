70fd92a54c9fde5a71060782524692ef135025df
alex echaniz <10alex.echa@gmail.com>
2019-08-28 22:38:42 +0200

    klk

--- index.js
+++ index.js
@@ -742,7 +742,7 @@ var object = [
         "date": update.date,
         "X": update.X,
         "Y": update.Y,
-        "img1": raster,
+        "img1": blob,
         //"img": { "data": update.img.data, "Type": update.img.contentType },
         "observation": update.observation,
         "imgUrl1": res ,
